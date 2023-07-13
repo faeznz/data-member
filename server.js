8e785a4dea293e08a10c2684d270ebc8c02e9519
Your Name <you@example.com>
2023-07-13 18:12:24 +0700

    p

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.use((req, res, next) => {
  });
 
 // Membuat koneksi ke MongoDB Atlas
-// const uri = 'mongodb+srv://faeznz:faeznz@data.h3xudui.mongodb.net/?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://faeznz:faeznz@data.h3xudui.mongodb.net/?retryWrites=true&w=majority';
 // const uri = 'mongodb://127.0.0.1:27017/datamember';
-const uri = 'mongodb://0.0.0.0:27017/datamember';
+// const uri = 'mongodb://0.0.0.0:27017/datamember';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
